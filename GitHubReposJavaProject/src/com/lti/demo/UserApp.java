9982608c1991ffa278dd2f3a3b76b2346447be0b
MihirGondhalekar <gondhalekarmihir@gmail.com>
2021-09-15 15:30:02 +0530

    third commit

--- GitHubReposJavaProject/src/com/lti/demo/UserApp.java
+++ GitHubReposJavaProject/src/com/lti/demo/UserApp.java
@@ -5,6 +5,7 @@ public class UserApp {
 	public static void main(String[] args) {
 		Product p = new Product(101,"mobile",5000);
 		System.out.println(p);
-		System.out.println();
+		System.out.println("abc");
+		System.out.println("pqr");
 	}
 }
